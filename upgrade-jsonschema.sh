2f7a59a2c8ea35860af97b6ce5f9181342d0d9ba
Juan Cruz Viotti <jv@jviotti.com>
2026-05-07 13:31:37 -0400

    Add a convenience dependency bump script
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ upgrade-jsonschema.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+cd "$SCRIPT_DIR"
+
+cd vscode
+VERSION="$(npm view @sourcemeta/jsonschema version)"
+cd -
+
+if [ -z "$VERSION" ]
+then
+    echo "Failed to determine latest version of @sourcemeta/jsonschema" >&2
+    exit 1
+fi
+
+BRANCH_VERSION="$(echo "$VERSION" | tr '.' '-')"
+BRANCH="upgrade-jsonschema-$BRANCH_VERSION"
+
+if git show-ref --verify --quiet "refs/heads/$BRANCH"
+then
+    git branch --delete --force "$BRANCH"
+fi
+
+git switch --create "$BRANCH"
+
+cd vscode
+npm install "@sourcemeta/jsonschema@$VERSION"
+cd -
+
+git add vscode/package.json vscode/package-lock.json
+git ci --message "Upgrade JSON Schema to v$VERSION"
+
+pr
